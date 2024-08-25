335159726aebcaffc72e3bd2af38bf9693b1dba8
Annexode <annexode@gmail.com>
2024-08-25 21:33:21 +0200

    JavaScript exercise folder

---
+++ app1.js
@@ -0,0 +1,13 @@
+const number1 = 20;
+const number2 =10;
+
+const add = number1 + number2;
+const sub = number1 - number2;
+const mult = number1 * number2;
+const div = number1 / number2;
+
+console.log(add);
+console.log(sub);
+console.log(div);
+console.log(div);
+
